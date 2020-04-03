68498fc644c790198c32379739532fcefa4ea2b5
tetsuya <tetsuya@users.noreply.github.com>
2020-04-03 14:59:14 +0900

    Fix deprecated interpolation-only expression see: https://www.terraform.io/upgrade-guides/0-12.html#first-class-expressions

--- heroku.tf
+++ heroku.tf
@@ -3,8 +3,8 @@ variable "api_key" {}
 variable "app_name" {}
 
 provider "heroku" {
-  api_key = "${var.api_key}"
-  email   = "${var.email}"
+  api_key = var.api_key
+  email   = var.email
 }
 
 resource "heroku_config" "common" {
@@ -15,7 +15,7 @@ resource "heroku_config" "common" {
 }
 
 resource "heroku_app" "default" {
-  name   = "${var.app_name}"
+  name   = var.app_name
   region = "us"
 
   buildpacks = [
@@ -23,23 +23,22 @@ resource "heroku_app" "default" {
   ]
 }
 
-
 resource "heroku_app_config_association" "default" {
-  app_id = "${heroku_app.default.id}"
-  vars = "${heroku_config.common.vars}"
+  app_id = heroku_app.default.id
+  vars = heroku_config.common.vars
 }
 
 resource "heroku_addon" "database" {
-  app  = "${heroku_app.default.name}"
+  app  = heroku_app.default.name
   plan = "jawsdb:kitefin"
 }
 
 resource "heroku_addon" "monitoring" {
-  app  = "${heroku_app.default.name}"
+  app  = heroku_app.default.name
   plan = "scout:chair"
 }
 
 resource "heroku_addon" "error-monitoring" {
-  app  = "${heroku_app.default.name}"
+  app  = heroku_app.default.name
   plan = "sentry:f1"
 }
